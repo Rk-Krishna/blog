f99d9743cda7e0d2ef90d7c76c6da81e59c238f6
krishna <krishna,rk2021@vitstuden.ac.in>
2024-09-11 21:47:19 +0530

    first commit

---
+++ user_blog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+
+import requests
+from django.shortcuts import render
+
+def get_data_from_other_blog(api_url):
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+def blog_data_view(request):
+    api_url = 'http://localhost:8000/api/blogposts/'
+    blog_data = get_data_from_other_blog(api_url)
+    
+    return render(request, 'blog_template.html', {'data': blog_data})
